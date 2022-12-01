85e1f90c93ad155d7a515ab9b9a337c351055ffb
unknown <asarebrave@gmail.com>
2022-12-01 15:59:12 +0100

    feat: added icon the title bar and some few features

---
+++ src/js/url.js
@@ -0,0 +1,7 @@
+function goToResume() {
+        window.open("https://docs.google.com/document/d/11V5PLv4yF7sH5IXDpZq1aZB-lFysO4TE/edit#heading=h.3znysh7");
+}
+
+function goToGithub(){
+    window.open("https://github.com/AsareBrave")
+}
