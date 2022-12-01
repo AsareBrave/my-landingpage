36edfa89bbca429bd8703bfa9321aa1a51a0d9da
unknown <asarebrave@gmail.com>
2022-12-01 20:55:38 +0100

    refactor: made a refactor to the code base

--- src/js/url.js
+++
@@ -1,7 +0,0 @@
-function goToResume() {
-        window.open("https://docs.google.com/document/d/11V5PLv4yF7sH5IXDpZq1aZB-lFysO4TE/edit#heading=h.3znysh7");
-}
-
-function goToGithub(){
-    window.open("https://github.com/AsareBrave")
-}
